9ea0627d0273b1b298d8c2825b1ad7e6e814a131
Jordan Skomal <jskomal@gmail.com>
2022-04-09 11:06:36 -0600

    Build Welcome Screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Link, Route, Switch } from 'react-router-dom'
 import './App.css'
+import welcomeGif from './images/rainruins.gif'
 
-function App() {
-  return (
-    <>
-      <h1>Hello</h1>
-    </>
-  )
+export class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route path='/'>
+          <section className='welcome-view'>
+            <img className='welcome-gif' src={welcomeGif} alt='Rainy ruins' />
+            <h1 className='main-title'>hail and well met, traveler...</h1>
+            <Link to='/character-select'>
+              <button className='enter-button'>roll for initiative</button>
+            </Link>
+          </section>
+        </Route>
+        <Route path='/character-select'>
+          <section className='character-view'></section>
+        </Route>
+      </Switch>
+    )
+  }
 }
 
 export default App
