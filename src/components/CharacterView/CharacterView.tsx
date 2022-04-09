5a64f442a30e69f8ddbc83c77ab6384e7f0b5f2a
Francesca McConnell <fmcconnell59@gmail.com>
2022-04-09 15:12:22 -0600

    Map through characters

--- src/components/CharacterView/CharacterView.tsx
+++ src/components/CharacterView/CharacterView.tsx
@@ -1,9 +1,41 @@
-import React, { Component } from 'react'
+import React from 'react'
+import CharacterCard from '../CharacterCard/CharacterCard'
 
-export class CharacterView extends Component {
-  render() {
-    return <div>CharacterView</div>
-  }
+interface CharacterStats {
+  class: string
+  name: string
+  HP: number
+  AC: number
+  weapon: string
+  weaponDmg: string
+  toHit: number
+  initiative: number
+  specialAbility: string
+}
+
+const CharacterView = ({ characters : any }) => {
+  const characterCards = characters.map(character => {
+    return <CharacterCard
+      name={character.name}
+      class={character.class}
+      HP={character.HP}
+      AC={character.AC}
+      weapon={character.weapon}
+      weaponDmg={character.weaponDmg}
+      toHit={character.toHit}
+      initiative={character.initiative}
+      specialAbility={character.specialAbility}
+      />
+  })
+  
+  return (
+    <section className="character-page">
+      <h1 className='choose-text'>Choose your champion!</h1>
+      <div className='character-container'>
+        {characterCards}
+      </div>
+    </section>
+  )
 }
 
 export default CharacterView
