d9cab134ccd9095b3aece1b20b52f670e6954524
Francesca McConnell <fmcconnell59@gmail.com>
2022-04-11 16:11:46 -0600

    Add champion details to character card

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -2,12 +2,25 @@ import React from 'react'
 import './CharacterCard.css'
 import { CharacterStats } from '../../App'
 
-const CharacterCard = ({ DnDClass, name, HP, AC, weapon, weaponDmg, toHit, initiative, bonusDmg, specialAbility, portrait } : CharacterStats) => {
+const CharacterCard = ({ DnDClass, name, HP, AC, weapon, weaponDmg, toHit, initiative, bonusDmg, specialAbility, portrait, isClicked, toggleDetails } : CharacterStats) => {
+  console.log(isClicked)
   return (
-    <section className='character-card'>
-      <img src={portrait} alt={`${name} portrait`}/>
+    <section className='character-card' onClick={toggleDetails}>
+      {!isClicked && <img src={portrait} alt={`${name} portrait`}/>}
       <h1 className='champion-name'>{name}</h1>
       <h2 className='champion-class'>{DnDClass}</h2>
+      {isClicked && (<div className="champion-details">
+        <h3>Hit Points: {HP}</h3>
+        <h3>Armor Class: {AC}</h3>
+        <h3>Weapon: {weapon}</h3>
+        <p>Weapon Damage: {weaponDmg}</p>
+        <p>Hit Bonus: {toHit}</p>
+        <p>Damage Bonus: {bonusDmg}</p>
+        <p>Initiative Bonus: {initiative}</p>
+        <p>Bonus Action: {specialAbility}</p>
+      </div>
+      )
+      }
     </section>
   )
 }
