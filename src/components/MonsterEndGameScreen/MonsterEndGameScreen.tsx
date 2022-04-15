11e791c17e057a8fc079b2ffc3b93edce6cf7a71
Francesca McConnell <fmcconnell59@gmail.com>
2022-04-15 16:46:08 -0600

    Add html elements for losing page

--- src/components/MonsterEndGameScreen/MonsterEndGameScreen.tsx
+++ src/components/MonsterEndGameScreen/MonsterEndGameScreen.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './MonsterEndGameScreen.css'
+import death from '../../images/deathgif.gif'
 
 const MonsterEndGameScreen = () => {
   return (
     <div className='monster-win-area'>
-      <p>You suck, loser!</p>
-      <Link to='/character-select'>
-        <button>Play Again?</button>
-      </Link>
+      <img className='lose-gif' src={death} alt='you lose' />
+      <div className='losing-text-container'>
+        <p className='losing-text'>You have fallen...</p>
+        <Link to='/character-select'>
+          <button className='losing-button'>Redeem your honor?</button>
+        </Link>
+      </div>
     </div>
   )
 }
