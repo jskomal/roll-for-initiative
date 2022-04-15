ba6de30b4c824d8f55bd432523ba6ed69c8e925d
Jordan Skomal <jskomal@gmail.com>
2022-04-15 12:01:45 -0600

    Upgrade to React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+const container = document.getElementById('root')
+const root = createRoot(container!)
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
