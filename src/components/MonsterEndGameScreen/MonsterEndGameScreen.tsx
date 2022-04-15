18f47ad3d729437dde88ccd0be4076939daa65c2
Francesca McConnell <fmcconnell59@gmail.com>
2022-04-15 17:21:06 -0600

    Fix losing end styling

--- src/components/MonsterEndGameScreen/MonsterEndGameScreen.tsx
+++ src/components/MonsterEndGameScreen/MonsterEndGameScreen.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './MonsterEndGameScreen.css'
-import death from '../../images/deathgif.gif'
+import death from '../../images/fallenwarrior.gif'
 
 const MonsterEndGameScreen = () => {
   return (
