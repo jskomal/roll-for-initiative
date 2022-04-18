2cd30e43a4386386ecca40e36119b6771601cc8a
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-04-18 17:38:24 -0600

    Home page cypress testing

---
+++ cypress/integration/Homepage-spec.js
@@ -0,0 +1,65 @@
+describe('Main view Tests', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://www.dnd5eapi.co/api/monsters/zombie', {
+      response: 200,
+      fixture: 'monster.json'
+    })
+  })
+
+  it('should render a welcome message', () => {
+    cy.visit('http://localhost:3000/')
+      .get('.main-title')
+      .contains('hail and well met, traveler...')
+      .get('.enter-button')
+      .contains('roll for initiative')
+  })
+
+  it('should be able to click roll for initiative', () => {
+    cy.get('.enter-button')
+      .contains('roll for initiative')
+      .click()
+      .url()
+      .should('eq', 'http://localhost:3000/character-select')
+      .get('.choose-text')
+      .contains('Choose your champion!')
+  })
+
+  it('should be able to get instructions on how to play', () => {
+    cy.visit('http://localhost:3000/')
+      .get('.howToPlay-button')
+      .contains('how to play')
+      .click()
+      .get('.howToPlay-title')
+      .contains('Welcome to Roll for Initiative!')
+  })
+
+  it('should be able to go from instructions to choose your champion', () => {
+    cy.get('.proceed-to-characters-button')
+      .contains('choose your champion')
+      .click()
+      .get('.choose-text')
+      .contains('Choose your champion!')
+  })
+
+  it('should be able to see game credits', () => {
+    cy.visit('http://localhost:3000/')
+      .get('#credits')
+      .contains('credits')
+      .click()
+      .get('.howToPlay-title')
+      .contains('Credits')
+  })
+
+  it('should be able to go from credits back to home', () => {
+    cy.get('.howToPlay-title')
+      .contains('Credits')
+      .get('button')
+      .contains('Home')
+      .click()
+      .get('.main-title')
+      .contains('hail and well met, traveler...')
+      .get('.enter-button')
+      .contains('roll for initiative')
+    
+  })
+})
